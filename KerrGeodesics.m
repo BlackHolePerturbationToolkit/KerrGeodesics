36dbe76892661eb1adda7450ad54f1e699ae62fb
Barry Wardell <barry.wardell@gmail.com>
2019-03-13 23:38:59 -0400

    Add variables for providing package information

--- KerrGeodesics.m
+++ KerrGeodesics.m
@@ -29,7 +29,63 @@ KerrGeoIBSO::usage = "KerrGeoISBO[a,x] returns the location of the innermost bou
 KerrGeoSeparatrix::usage = "KerrGeoSeparatrix[a,e,x] returns the value of p at the separatrix"
 KerrGeoBoundOrbitQ::usage = "KerrGeoBoundOrbitQ[a,p,e,x] tests if the orbital parameters correspond to a bound orbit."
 
+$KerrGeodesicsInformation::usage = "$KerrGeodesicsInformation is a list of rules that gives information about the version of the KerrGeodesics package you are running.";
+$KerrGeodesicsInstallationDirectory::usage = "$KerrGeodesicsInstallationDirectory gives the top-level directory in which the KerrGeodesics package is installed.";
+
+$KerrGeodesicsVersionNumber::usage = "$KerrGeodesicsVersionNumber is a real number which gives the current version number for the KerrGeodesics package.";
+$KerrGeodesicsReleaseNumber::usage = "$KerrGeodesicsReleaseNumber is an integer which gives the current release number for the KerrGeodesics package.";
+$KerrGeodesicsVersion::usage = "$KerrGeodesicsVersionNumber is a string that gives the version of the KerrGeodesics package you are running.";
+
 Begin["`Private`"];
+
+(**********************************************************)
+(* Package version information                            *)
+(**********************************************************)
+
+$KerrGeodesicsInstallationDirectory = FileNameDrop[FindFile["KerrGeodesics`"], -2];
+
+$KerrGeodesicsVersionNumber        = 1.0;
+$KerrGeodesicsReleaseNumber        = 0;
+
+$KerrGeodesicsVersion :=
+ Module[{path, version, release, buildid, gitrev, gitdir},
+  path = $KerrGeodesicsInstallationDirectory;
+  version = ToString[NumberForm[$KerrGeodesicsVersionNumber, {Infinity, 1}]];
+  release = ToString[$KerrGeodesicsReleaseNumber];
+
+  buildid = Quiet@ReadList[FileNameJoin[{path, "BUILD_ID"}], "String"];
+  If[SameQ[buildid, $Failed],
+    buildid = "";
+  ,
+    buildid = " (" <> First[buildid] <> ")";
+  ];
+
+  (* First, check for a GIT_REVISION file. If it exists, use its contents as the revision. *)
+  gitrev = Quiet@ReadList[FileNameJoin[{path, "GIT_REVISION"}],"String"];
+
+  (* Otherwise, try to determine the git revision directly *)
+  If[SameQ[gitrev, $Failed],
+    gitdir = FileNameJoin[{path, ".git"}];
+    If[FileType[gitdir] === Directory,
+      gitrev = Quiet@ReadList["!git --git-dir "<>gitdir<>" rev-parse HEAD", String];
+      If[gitrev === {}, gitrev = $Failed];
+    ];
+  ];
+
+  (* If it worked, ReadList returns a list but we just want the first element (line) *)
+  If[Head[gitrev] === List, gitrev = First[gitrev]];
+
+  (* Check we have a git revision and otherwise give up trying *)
+  If[Head[gitrev] === String && StringMatchQ[gitrev, RegularExpression["[0-9a-f]{5,40}"]], gitrev = " (" <> gitrev <> ")", gitrev = ""];
+
+  version <> "." <> release <> buildid <> gitrev
+]
+
+$KerrGeodesicsInformation :=
+  {"InstallationDirectory" -> $KerrGeodesicsInstallationDirectory,
+   "Version" -> $KerrGeodesicsVersion,
+   "VersionNumber" -> $KerrGeodesicsVersionNumber,
+   "ReleaseNumber" -> $KerrGeodesicsReleaseNumber}
 
 
 (* ::Chapter::Closed:: *)
