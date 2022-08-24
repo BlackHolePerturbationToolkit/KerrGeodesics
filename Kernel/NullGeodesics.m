6648424717007551cceeb62449409ab3c7289776
NielsW <niels.warburton@ucd.ie>
2022-08-24 12:03:22 +0100

    Create new file for null geodesic functions

---
+++ Kernel/NullGeodesics.m
@@ -0,0 +1,31 @@
+(* ::Package:: *)
+
+(* ::Title:: *)
+(*NullGeodesics subpackage*)
+
+
+(* ::Section:: *)
+(*Define usage for public functions*)
+
+
+BeginPackage["KerrGeodesics`NullGeodesics`"]
+
+KerrNullEnergy::usage = "test fuction"
+
+Begin["`Private`"];
+
+
+(* ::Section:: *)
+(*Constants of Motion*)
+
+
+KerrNullEnergy[]:=Print["It works!"]
+
+
+(* ::Section:: *)
+(*Close the package*)
+
+
+End[];
+
+EndPackage[];
